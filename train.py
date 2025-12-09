bb2d7809809e28066e3ad75e04d19a81b8ecfd6f
filiperobotic <filipe.rolim@gmail.com>
2025-12-09 12:37:12 -0300

    aaa

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ print(model)
 #train
 # model.train(data="coco.yaml",device=[7],epochs=100)  # train the model
 #model.train(data="coco.yaml",device=[4],epochs=100)  # train the model
-model.train(data="./data.yaml",imgsz=imgsz,device=0,batch=16,epochs=100,project="runs/train", name="snn_oxford_tower_custom_train")  # train the model
+model.train(data="./data.yaml",imgsz=imgsz,device=0,batch=4,epochs=100,project="runs/train", name="snn_oxford_tower_custom_train")  # train the model
 
 #TEST
 # model = YOLO('runs/detect/train1/weights/last.pt')  # load a pretrained model (recommended for training)
