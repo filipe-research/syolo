2e3c3608b5f24d43258f47462154676e20b46121
filiperobotic <filipe.rolim@gmail.com>
2025-12-09 12:49:09 -0300

    bsaa

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ iter = 125 #the iters of an epoch
 
 i = 0
 
-model = YOLO('/path/to/weight/.pt')
+model = YOLO('/home/pesquisador/pesquisa/filipe/model_compress/runs/train/snn_oxford_tower_custom_train3/weights/best.pt')
 
 def forward_hook_fn(module, input, output):  # 计算每一层的发放率
     global i
@@ -31,9 +31,13 @@ for n, m in model.named_modules():
         m.name = n
         m.register_forward_hook(forward_hook_fn)
 
-model.val(data="coco.yaml",device=[2])
+#model.val(data="coco.yaml",device=[2])
+results = model.val(data="data.yaml",device=[0], split='test')
 print("fire:",fr_dict) #the firing rate of each layer
 
+# Exibe os principais resultados
+print(f"mAP50: {results.box.map50:.4f}")
+print(f"mAP50-95: {results.box.map:.4f}")
 
 
 
